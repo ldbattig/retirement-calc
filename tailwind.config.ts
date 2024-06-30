94d452c663d10fc34029407f44b700ec84877dfd
ldbattig <ldbattig@ncsu.edu>
2024-06-30 15:01:43 -0400

    remove tailwind config changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,6 @@ export default {
 	theme: {
 		extend: {
       colors: {
-        // primary: 'var(--color-primary)',
-        secondary: 'var(--color-secondary)',
-        background: 'var(--color-background)',
-        text: 'var(--color-text)',
         // flowbite-svelte
         primary: {
           50: '#FFF5F2',
